a039e56823964fa7ffcbfb94e69c74e0b285cbdd
Sirichok jaimano <sirichok2315@gmail.com>
2026-07-17 15:09:21 +0700

    Update quasar.comfig.js with notify in plugin

--- quasar.config.js
+++ quasar.config.js
@@ -80,7 +80,9 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
