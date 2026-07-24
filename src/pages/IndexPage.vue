10071998d96318e8a8ae14878c17ae4a76524c53
Sirichok jaimano <sirichok2315@gmail.com>
2026-07-24 14:00:19 +0700

    Edit Colloring form in IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,74 +1,72 @@
 <template>
- <div class="q-pa-md" style="max-width: 400px">
-  <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-    <q-input
-      filled
-      v-model="name"
-      label="Your name *"
-      hint="Name and surname"
-      lazy-rules
-      :rules="[val => (val && val.length > 0) || 'Please type something']"
-    />
+ <div class="q-pa-md">
+  <div class="q-gutter-y-md column" style="max-width: 300px">
+    <q-input color="purple-12" v-model="text" label="Label">
+      <template v-slot:prepend>
+        <q-icon name="event" />
+      </template>
+    </q-input>
+
+    <q-input color="teal" filled v-model="text" label="Label">
+      <template v-slot:prepend>
+        <q-icon name="event" />
+      </template>
+    </q-input>
 
     <q-input
+      color="grey-3"
+      label-color="orange"
+      outlined
+      v-model="text"
+      label="Label"
+    >
+      <template v-slot:append>
+        <q-icon name="event" color="orange" />
+      </template>
+    </q-input>
+
+    <q-input
+      color="lime-11"
+      bg-color="green"
       filled
-      type="number"
-      v-model.number="age"
-      label="Your age *"
-      lazy-rules
-      :rules="[
-        val => (val !== null && val !== '') || 'Please type your age',
-        val => (val > 0 && val < 100) || 'Please type a real age'
-      ]"
-    />
+      v-model="text"
+      label="Label"
+    >
+      <template v-slot:prepend>
+        <q-icon name="event" />
+      </template>
+    </q-input>
 
-    <q-toggle v-model="accept" label="I accept the license and terms" />
+    <q-input color="teal" outlined v-model="text" label="Label">
+      <template v-slot:append>
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg" />
+        </q-avatar>
+      </template>
+    </q-input>
 
-    <div>
-      <q-btn label="Submit" type="submit" color="primary" />
-      <q-btn
-        label="Reset"
-        type="reset"
-        color="primary"
-        flat
-        class="q-ml-sm"
-      />
-    </div>
-  </q-form>
+    <q-input
+      color="orange"
+      standout
+      bottom-slots
+      v-model="text"
+      label="Label"
+      counter
+      clearable
+    >
+      <template v-slot:prepend>
+        <q-icon name="place" />
+      </template>
+      <template v-slot:append>
+        <q-icon name="favorite" />
+      </template>
+
+      <template v-slot:hint> Field hint </template>
+    </q-input>
+  </div>
 </div>
 </template>
 
 <script setup>
-  import { useQuasar } from 'quasar'
-  import { ref } from 'vue'
-
-  const $q = useQuasar()
-
-  const name = ref(null)
-  const age = ref(null)
-  const accept = ref(false)
-
-  function onSubmit() {
-    if (accept.value !== true) {
-      $q.notify({
-        color: 'red-5',
-        textColor: 'white',
-        icon: 'warning',
-        message: 'You need to accept the license and terms first'
-      })
-    } else {
-      $q.notify({
-        color: 'green-4',
-        textColor: 'white',
-        icon: 'cloud_done',
-        message: 'Submitted'
-      })
-    }
-  }
-
-  function onReset() {
-    name.value = null
-    age.value = null
-    accept.value = false
-  }
+ 
 </script>
